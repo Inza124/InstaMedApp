790e4b764f799f4d7499f0673cfc7d275bc327d1
Inza124 <piotrstachaczynski.prv@gmail.com>
2018-03-23 15:29:35 +0100

    dbo changes #1

--- InstaMedApp/Controllers/AccountController.cs
+++ InstaMedApp/Controllers/AccountController.cs
@@ -221,7 +221,7 @@ namespace InstaMedApp.Controllers
             ViewData["ReturnUrl"] = returnUrl;
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, FirstName = model.FirstName, LastName = model.LastName, Telephone = model.Telephone, JoinDate = DateTime.Now };
                 var result = await _userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
--- InstaMedData/Models/ApplicationUser.cs
+++ InstaMedData/Models/ApplicationUser.cs
@@ -8,12 +8,15 @@ namespace InstaMedData.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        //public new String Id { get; set; }
-        //public String FirstName { get; set; }
-        //public String LastName { get; set; }
-        //public String Telephone { get; set; }
-        //public new String Email { get; set; }
-        //public String Password { get; set; }
-        //public DateTime JoinDate { get; set; }
+        public new String Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Telephone { get; set; }
+        public new String Email { get; set; }
+        public String Password { get; set; }
+        public DateTime JoinDate { get; set; }
+
+        // public Role UserRole { get; set; }
+        // public ICollection<Test> Tests { get; set; }
     }
 }
